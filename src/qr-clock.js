791c0ac77bcac1abbe166aac383948c44e90b281
Yehuda B <yehudab@gmail.com>
2021-01-01 21:19:26 +0200

    Use requestAnimationFrame inseatd of interval

--- src/qr-clock.js
+++ src/qr-clock.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import QRCode from 'qrcode.react'
-import { useInterval } from './hooks/set-interval'
 import moment from 'moment'
 import withSizes from 'react-sizes'
 import GitHubForkRibbon from 'react-github-fork-ribbon'
@@ -9,13 +8,27 @@ const getTime = () => moment().format('YYYY-MM-DDTHH:mm:ss');
 
 const QRClock = ({ width, height }) => {
   let [time, setTime] = useState(getTime())
+  let last = 0;
 
-  useInterval(() => {
-    setTime(getTime())
-  }, 1000)
+  // From: https://css-tricks.com/using-requestanimationframe-with-react-hooks/
+  const requestRef = React.useRef();
+  
+  const animate = now => {
+    if (now - last >= 1000) {
+      setTime(getTime());
+      last = now;
+    }
+    requestRef.current = requestAnimationFrame(animate);
+  }
+  
+  React.useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []); // Make sure the effect runs only once
+
   return (
     <div className='main-container'>
-      <div style={{ color: '#000', height: 25 }}>{ `QR-Clock by kaelhem ©2019` }</div>
+      <div style={{ color: '#000', height: 25 }}>{ `QR-Clock by kaelhem ©2019, Yehuda B. ©2021` }</div>
       <div className="humak-date">{ time.split('T')[0] }</div>
       <div className="humak-time">{ time.split('T')[1] }</div>
       <div className='clock-container'>
